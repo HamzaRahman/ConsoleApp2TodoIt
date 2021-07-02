76ffe1788d7d0d9daa54a1197e77fa5e2f09e5e9
Hamza <hamza.rahman2016@outlook.com>
2021-07-02 21:26:19 +0500

    Task 8 d

--- ConsoleApp2TodoIt/Data/People.cs
+++ ConsoleApp2TodoIt/Data/People.cs
@@ -19,5 +19,26 @@ namespace ConsoleApp2TodoIt.Data
         {
             return peoples;
         }
+        //Task 8 d
+        public Person FindByID(int PersonID)
+        {
+            //We are taking this person because we have to return something at the end if our wanted person is not found
+            //To know whether the function returned the person we wanted or this new one, we are setting its ID to 0
+            //Because all other persons ID will start form one and we will know if its zero than not found, else if its greater than
+            //zero, its found
+            Person person = new Person(0, "MM", "MM");
+            //Here we run a foreach loop on peoples array
+            foreach (var p in peoples)
+            {
+                //Then we compare our wanted ID with every persons ID
+                if (p.PersonID == PersonID)
+                {
+                    //if its found then it will be returned
+                    return p;
+                }
+            }
+            //else the 0 ID person we created will be returned
+            return person;
+        }
     }
 }
