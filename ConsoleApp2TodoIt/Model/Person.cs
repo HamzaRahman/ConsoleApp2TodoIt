e9930a8c8cb17dba6175436feed7233822ecc2f1
Hamza <hamza.rahman2016@outlook.com>
2021-07-02 20:05:33 +0500

    Task 3b

--- ConsoleApp2TodoIt/Model/Person.cs
+++ ConsoleApp2TodoIt/Model/Person.cs
@@ -6,8 +6,17 @@ namespace ConsoleApp2TodoIt.Model
 {
     class Person
     {
+        //Task 3a
         readonly int personId;
         string firstName;
         string lastName;
+        //Task 3b
+        public Person(int pID, string firstName, string lastName)
+        {
+            //Initializing/creating object.
+            this.personId = pID;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
     }
 }
