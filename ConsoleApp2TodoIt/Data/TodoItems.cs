984f477000397313ce38df232c5e54d33f2ed2f6
Hamza <hamza.rahman2016@outlook.com>
2021-07-02 22:04:28 +0500

    Task 9 a

--- ConsoleApp2TodoIt/Data/TodoItems.cs
+++ ConsoleApp2TodoIt/Data/TodoItems.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp2TodoIt.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,7 @@ namespace ConsoleApp2TodoIt.Data
 {
     public class TodoItems
     {
+        //Task 9 a
+        static Todo[] todoitems = new Todo[0];
     }
 }
