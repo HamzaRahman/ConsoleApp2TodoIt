d4b8e89eaed5b11eecfec6188f6f60c029e5db4c
Hamza <hamza.rahman2016@outlook.com>
2021-07-02 20:54:03 +0500

    Task 7 d

---
+++ ConsoleApp2TodoIt.UnitTests/TodoSequencer-UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp2TodoIt.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ConsoleApp2TodoIt.UnitTests
+{
+    public class TodoSequencer_UnitTest
+    {
+        [Fact]
+        public void TodoIDTest()
+        {
+            int ID = TodoSequencer.nextTodoid();
+            Assert.Equal(1, ID);
+        }
+        [Fact]
+        public void ResetTest()
+        {
+            TodoSequencer.reset();
+        }
+    }
+}
