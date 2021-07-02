8ed3a59f0f74b8ecc763e37682bc994be4c6ecc0
Hamza <hamza.rahman2016@outlook.com>
2021-07-02 20:03:36 +0500

    Task 3,a

---
+++ ConsoleApp2TodoIt/Model/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2TodoIt.Model
+{
+    class Person
+    {
+        readonly int personId;
+        string firstName;
+        string lastName;
+    }
+}
