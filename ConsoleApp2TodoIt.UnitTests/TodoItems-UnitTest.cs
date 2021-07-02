6e87b6c6ac6e12f42d0fae8be449284801c6a933
Hamza <hamza.rahman2016@outlook.com>
2021-07-02 22:26:39 +0500

    Task 10 e

--- ConsoleApp2TodoIt.UnitTests/TodoItems-UnitTest.cs
+++ ConsoleApp2TodoIt.UnitTests/TodoItems-UnitTest.cs
@@ -47,7 +47,90 @@ namespace ConsoleApp2TodoIt.UnitTests
             Todo ps = todoItems.AddTodo(Desc, true, pr);
             Assert.Equal(1, ps.todoID);
         }
-
-
+        //Task 10 a
+        [Theory]
+        [InlineData(true)]
+        public void FindByDoneStatusTest(bool s)
+        {
+            bool actualresult = false;
+            Person pr = new Person(1, "dd", "ee");
+            //Here we add a person first in the Todo array
+            todoItems.AddTodo("DDD", true, pr);
+            //Then we find for Todo items which have done status true
+            Todo[] t = todoItems.FindByDoneStatus(s);
+            foreach (var c in t)
+            {
+                //Now we verify/test if the returned array has all and only true done status todo items
+                if (c.Done == s)
+                {
+                    actualresult = true;
+                }
+            }
+            Assert.True(actualresult);
+        }
+        //Task 10 b
+        [Theory]
+        [InlineData(1)]
+        public void FindByAssignee(int ID)
+        {
+            bool actualresult = false;
+            Person pr = new Person(1, "dd", "ee");
+            todoItems.AddTodo("DDD", true, pr);
+            //Then we find for Todo items which have assignee ID equal to 1
+            Todo[] t = todoItems.FindByAssignee(ID);
+            foreach (var c in t)
+            {
+                //Now we verify/test if the returned array has all and only assignee ID equal to 1 todo items
+                //Assert.Equal(1, c.Assignee.PersonID);
+                if (c.Assignee.PersonID == ID)
+                {
+                    actualresult = true;
+                }
+            }
+            Assert.True(actualresult);
+        }
+        //Task 10 c
+        //Here we take a Person for input search, (we are not going to add it in the array)
+        Person FBA = new Person(1, "dd", "ee");
+        [Fact]
+        public void FindByAssigneeTest()
+        {
+            bool actualresult = false;
+            Person pr = new Person(1, "dd", "ee");
+            //Here we add a person first in the Todo array
+            todoItems.AddTodo("DDD", true, pr);
+            //Then we find for Todo items which have assignee equal to FBA (Input search data)
+            Todo[] t = todoItems.FindByAssignee(FBA);
+            foreach (var c in t)
+            {
+                //Now we verify/test if the returned array has all and only assignee equal to FBA (Input search data) todo items
+                if ((c.Assignee.FirstName == FBA.FirstName)
+                    && (c.Assignee.LastName == FBA.LastName))
+                {
+                    actualresult = true;
+                }
+            }
+            Assert.True(actualresult);
+        }
+        //Task 10 d
+        [Fact]
+        public void FindByUnAssignedTodoItemsTest()
+        {
+            bool actualresult = false;
+            Person pr = new Person(1, "dd", "ee");
+            //Here we add a person first in the Todo array
+            todoItems.AddTodo("DDD", true, null);
+            //Then we find for Todo items which have assignee equal to FBA (Input search data)
+            Todo[] t = todoItems.FindUnassignedTodoItems();
+            foreach (var c in t)
+            {
+                //Now we verify/test if the returned array has all and only assignee equal to FBA (Input search data) todo items
+                if (c.Assignee == null)
+                {
+                    actualresult = true;
+                }
+            }
+            Assert.True(actualresult);
+        }
     }
 }
